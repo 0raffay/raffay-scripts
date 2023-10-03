ef546fcdd42c15a4e6439b751061539b928bddba
Abdul Raffay S <0.abdulraffay@gmail.com>
2023-10-03 10:41:56 +0500

    basic template edits

--- 0-basic-template/includes/footer-scripts.php
+++ 0-basic-template/includes/footer-scripts.php
@@ -3,9 +3,9 @@
 <!--==== LIBRARY FILES END =====-->
 
 <!--==== FANCYBOX START =====-->
-<!--==== FANCYBOX END =====-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+<!--==== FANCYBOX END =====-->
 
 <!--==== LOCAL FILES START =====-->
 <script src="assets/js/script.js"></script>
