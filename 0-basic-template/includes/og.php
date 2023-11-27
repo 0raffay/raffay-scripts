0743df456e88b9dda664eafab08b7e7dce13ca76
Abdul Raffay S <0.abdulraffay@gmail.com>
2023-11-27 11:33:35 +0500

    Basic updates

---
+++ 0-basic-template/includes/og.php
@@ -0,0 +1,52 @@
+<?php
+if (isset($_SERVER['HTTPS']))
+    $siteURL = "https://";
+else
+    $siteURL = "http://";
+$siteURL .= $_SERVER['HTTP_HOST'];
+
+$documentRoot = $_SERVER['DOCUMENT_ROOT'];
+$currentScriptPath = $_SERVER['SCRIPT_FILENAME'];
+$relativePath = str_replace($documentRoot, '', $currentScriptPath);
+$pathParts = explode("/", $relativePath);
+$folderName = $pathParts[1];
+
+// OG DATA:
+$siteName = "";
+$siteURL .= "/" . $folderName;
+
+$siteEmail = " ";
+$sitePhone = "";
+$siteAddress = "";
+
+
+$ogContent1 = "";
+$ogContent2 = "";
+$ogImgPath =  "$siteURL/assets/images/og.png";
+?>
+
+
+<!--==== OFINFO START ====-->
+<meta property="og:url" content="<?php echo $siteURL ?>">
+<meta property="og:type" content="website">
+<meta property="og:title" content="<?php echo $ogContent1 ?>">
+<meta property="og:description" content="<?php echo $ogContent2 ?>">
+<meta property="og:image" content="<?php echo $ogImgPath ?>">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="<?php echo $siteURL ?>">
+<meta property="twitter:url" content="<?php echo $siteURL ?>">
+<meta name="twitter:title" content="<?php echo $ogContent1 ?>">
+<meta name="twitter:description" content="<?php echo $ogContent2 ?>">
+<meta name="twitter:image" content="<?php echo $ogImgPath ?>">
+
+<meta property="og:image" content="<?php echo $ogImgPath ?>" />
+<meta property="og:image:secure_url" content="<?php echo $ogImgPath ?>" />
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="600" />
+<meta property="og:image:height" content="330" />
+<meta property="og:image:alt" content="<?php echo $ogContent1 ?>" />
+<meta name="image" property="og:image" content="<?php echo $ogImgPath ?>">
+<meta name="author" content="<?php echo $siteName ?>">
+<meta name="description" property="og:description" content="<?php echo $ogContent2 ?>">
+<!--==== OFINFO END ====-->
--- 0-basic-template/index.php
+++ 0-basic-template/index.php
@@ -2,8 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php include("includes/compatibility.php"); ?>
+    <?php include("includes/og.php"); ?>
 
     <!-- META TITLE AND DESCRIPTION -->
     <meta name="description" content="">
