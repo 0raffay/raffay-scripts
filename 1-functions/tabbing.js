45320aea710a88c0fd762cbc1c51db8d08968ef9
Abdul Raffay S <0.abdulraffay@gmail.com>
2023-12-21 17:02:37 +0500

    dev: tabbing v2

--- 1-functions/tabbing.js
+++ 1-functions/tabbing.js
@@ -25,3 +25,53 @@ function tabbing(
 }
 
 tabbing(".tabbingButtons button", ".tabbingPanels .tabbingPanel");
+
+
+
+
+function tabbingByTarget(buttonDataAttribute, targetsDataAttribute, initalHide = false, callback ) {
+    const selector = {
+        button: buttonDataAttribute ? buttonDataAttribute : "rf-tabbing-button",
+        target: targetsDataAttribute ? targetsDataAttribute : "rf-tabbing-target",
+    }
+    const buttons = $(`[${selector.button}]`);
+    const targets = $(`[${selector.target}]`);
+
+    if (initalHide) {
+        buttons.eq(0).addClass("active");
+        targets.eq(1).addClass("active");
+        targets.not(":nth-of-type(1)").hide();
+    }
+
+
+    const hideShow = (button, target) => {
+        //buttons
+        buttons.removeClass("active");
+        button.addClass("active");
+
+        //targets:
+        targets.removeClass("active").hide();
+        target.addClass("active").show();
+    }
+
+    buttons.click(function () {
+        if ($(this).hasClass("active")) {
+            return;
+        }
+
+        let thisButton = $(this);
+        let target = $(this).attr(`${selector.button}`);
+        targets.each(function () {
+            let thisTarget = $(this).attr(`${selector.target}`);
+            if (target == thisTarget) {
+                hideShow(thisButton, $(this));
+                if (callback) {
+                    callback(thisButton, $(this), thisTarget);
+                }
+                return;
+            }
+
+            thisButton.addClass("disabled");
+        })
+    })
+}
