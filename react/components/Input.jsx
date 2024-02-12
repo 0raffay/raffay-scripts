b554dc800e32e266091a81dd1fcdc8888e0d137b
Abdul Raffay S <0.abdulraffay@gmail.com>
2024-02-12 14:56:44 +0500

    add useform and update input

--- react/components/Input.jsx
+++ react/components/Input.jsx
@@ -8,6 +8,9 @@ export default function Input({
     type = "text",
     attributes = {},
     children,
+    name = "",
+    error,
+    errorClass,
 }) {
     return (
         <>
@@ -20,7 +23,7 @@ export default function Input({
             )}
             <div className="relative">
                 {children && children.length && (
-                    <span className="absolute top-1/2 -translate-y-1/2 left-3">
+                    <span className="absolute top-1/2 -translate-y-1/2 left-3 pointer-events-none">
                         {children}
                     </span>
                 )}
@@ -28,12 +31,22 @@ export default function Input({
                     className={`${className} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 focus:ring-1 focus:outline-none block w-full p-2.5  ${
                         children && children.length && "ps-[27px]"
                     }`}
+                    name={name}
                     type={type}
                     placeholder={placeholder}
                     onInput={onInput}
                     onChange={onChange}
                     {...attributes}
                 />
+                {error && (
+                    <p
+                        className={`absolute bottom-[-19px] text-[12px] font-medium text-[#E32626] ${
+                            errorClass ?? ""
+                        }`}
+                    >
+                        {error}
+                    </p>
+                )}
             </div>
         </>
     );
