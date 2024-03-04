a93601dea6542f42b4fb1c95d806e59c1e3c4059
Abdul Raffay S <0.abdulraffay@gmail.com>
2024-03-04 14:58:06 +0500

    full feature validate form

--- 1-functions/validateFormV3.js
+++ 1-functions/validateFormV3.js
@@ -4,11 +4,11 @@ class RFvalidateForm {
 			form = null,
 			inputs = null,
 			button = null,
-			specialFields = "rfvalidation-regex",
+			specialFields = "validate-regex",
+			validateCheckbox = true,
 			errorClass = "error",
 			disableClass = "disabled",
 			exclude = "exclude",
-			validateCheckbox = true,
 		},
 		success
 	) {
@@ -31,9 +31,9 @@ class RFvalidateForm {
 
 		this.form.setAttribute("novalidate", "true");
 
-		if (!inputs) {
-			this.setBasicInputs();
-		} else {
+		this.setBasicInputs();
+
+		if (inputs) {
 			this.setUserInputs(inputs);
 		}
 
@@ -41,13 +41,46 @@ class RFvalidateForm {
 			this.setCheckbox();
 		}
 
-		if (specialFields) {
-			this.setSpecialFields(specialFields);
-		}
+		this.setSpecialFields(specialFields);
 
 		this.valid = false;
 
 		this.#addEventListeners();
+	}
+
+	#addEventListeners() {
+		this.form.addEventListener("submit", (e) => {
+			e.preventDefault();
+			this.checkRegexValidity();
+			this.checkIfEmpty();
+			this.checkIfChecked();
+
+			if (this.valid) {
+				this.success(this.form);
+			}
+		});
+
+		this.inputs.forEach((i) => {
+			i.addEventListener("input", (e) => {
+				this.checkIfEmpty(e.target);
+			});
+		});
+
+		if (this.specialFields && this.specialFields.length) {
+			this.specialFields.forEach((s) => {
+				s.input.addEventListener("input", (e) => {
+					this.checkRegexValidity(s.input, s.regex);
+				});
+			});
+		}
+
+		if (this.checkboxes && this.checkboxes.length) {
+			this.checkboxes.forEach(c => {
+				c.addEventListener("input", (e) => {
+					this.checkIfChecked(c);
+				})
+			})
+		}
 	}
 
 	checkIfChecked(checkbox) {
@@ -75,54 +108,6 @@ class RFvalidateForm {
 			this.setValid();
 			return;
 		})
-	}
-
-	setCheckbox() {
-		const checkboxes = Array.from(this.form.querySelectorAll('input[type="checkbox"]'));
-		
-		if (!checkboxes || !checkboxes.length) {
-			return;
-		}
-
-		checkboxes.forEach(checkbox => {
-			this.checkboxes.push(checkbox);
-		});
-	}
-
-
-	#addEventListeners() {
-		this.form.addEventListener("submit", (e) => {
-			e.preventDefault();
-			this.checkRegexValidity();
-			this.checkIfEmpty();
-			this.checkIfChecked();
-
-			if (this.valid) {
-				this.success(this.form);
-			}
-		});
-
-		this.inputs.forEach((i) => {
-			i.addEventListener("input", (e) => {
-				this.checkIfEmpty(e.target);
-			});
-		});
-
-		if (this.specialFields) {
-			this.specialFields.forEach((s) => {
-				s.input.addEventListener("input", (e) => {
-					this.checkRegexValidity(s.input, s.regex);
-				});
-			});
-		}
-
-		if (this.checkboxes && this.checkboxes.length) {
-			this.checkboxes.forEach(c => {
-				c.addEventListener("input", (e) => {
-					this.checkIfChecked(c);
-				})
-			})
-		}
 	}
 
 	checkRegexValidity(input, regex) {
@@ -173,6 +158,10 @@ class RFvalidateForm {
 		});
 	}
 
+	checkErrorClass(input) {
+		return input.classList.contains(this.errorClass);
+	}
+
 	addErrorClass(input) {
 		input.classList.add(this.errorClass);
 	}
@@ -181,8 +170,36 @@ class RFvalidateForm {
 		input.classList.remove(this.errorClass);
 	}
 
-	checkErrorClass(input) {
-		return input.classList.contains(this.errorClass);
+	removeField(input) {
+		if (Array.isArray(input)) {
+			this.inputs.forEach(i => {
+				input.forEach((s) => {
+					if (i == s) {
+						this.inputs.splice(this.inputs.indexOf(i), 1);
+					}
+				});
+			})
+			return;
+		}
+		this.inputs.forEach(i => {
+			if (i == input) {
+				this.inputs.splice(this.inputs.indexOf(i), 1)
+			}
+		})
+		this.specialFields.forEach(i => {
+			if (i.input == input) {
+				this.specialFields = this.specialFields.filter(i => i.input !== input);
+			}
+		})
+	}
+
+	addRegexField(input, regex) {
+		this.removeField(input);
+		const f = {
+			input: input,
+			regex: new RegExp(regex),
+		}
+		this.specialFields.push(f);
 	}
 
 	setValid() {
@@ -199,47 +216,80 @@ class RFvalidateForm {
 	}
 
 	setBasicInputs() {
+		const checkIncludeStatus = (input) => {
+			return !input.classList.contains(this.exclude) &&
+				!input.hasAttribute(this.exclude) &&
+				!(input.type === "hidden");
+		}
 		const inputs = this.form.querySelectorAll(
-			"input[type='text'], input[type='email'], input[type='number'], input[type='tel'], input[type='date'], select, textarea, input[type='file'], input[type='password']"
+			"input[type='text'], input[type='email'], input[type='number'], input[type='date'], select, textarea, input[type='file'], input[type='password']"
 		);
+		if (!inputs.length) {
+			console.error("No input elements found in form.", this.form)
+			return;
+		}
 		inputs.forEach((i) => {
-			const includeStatus = !i.classList.contains(this.exclude) &&
-				!i.hasAttribute(this.exclude) &&
-				!(i.type === "hidden");
+			if (checkIncludeStatus(i)) {
+				this.inputs.push(i);
+			}
+		});
 
-			if (includeStatus) {
+		const specialFields = [
+			{
+				input: this.form.querySelector("input[type='tel']"),
+				regex: /^\d{8,}$/
+			},
+			{
+				input: this.form.querySelector("input[type='email']"),
+				regex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+			},
+		]
+		specialFields.forEach(s => {
+
+			if (s.input && checkIncludeStatus(s.input)) {
+				this.addRegexField(s.input, s.regex);
+			}
+		})
+	}
+
+	setUserInputs(inputSelector) {
+		const inputs = this.form.querySelectorAll(inputSelector);
+		if (!inputs.length) {
+			console.error("No input elements found with selector.")
+			return;
+		}
+		inputs.forEach((i) => {
+			const alreadyIncluded = this.inputs.indexOf(i);
+			if (!alreadyIncluded) {
 				this.inputs.push(i);
 			}
 		});
 	}
 
-	setUserInputs(inputSelector) {
-		const inputs = this.form.querySelectorAll(inputSelector);
-		inputs.forEach((i) => {
-			this.inputs.push(i);
+	setSpecialFields(specialFieldSelector) {
+		const specialFields = this.form.querySelectorAll(
+			`[${specialFieldSelector}]`
+		);
+		specialFields.forEach((s) => {
+			const include = s.getAttribute(specialFieldSelector);
+			if (include !== "") {
+				this.addRegexField(s, new RegExp(s.getAttribute(specialFieldSelector)))
+				return;
+			}
+			this.inputs.push(s);
 		});
 	}
 
-	setSpecialFields(specialFieldSelector) {
-		const specialField = this.form.querySelectorAll(
-			`[${specialFieldSelector}]`
-		);
-		if (specialField.length) {
-			this.inputs.forEach((i) => {
-				specialField.forEach((s) => {
-					if (i == s) {
-						this.inputs.splice(this.inputs.indexOf(i), 1);
-					}
-				});
-			});
-			specialField.forEach((s) => {
-				const _s = {
-					input: s,
-					regex: new RegExp(s.getAttribute(specialFieldSelector)),
-				};
-				this.specialFields.push(_s);
-			});
+	setCheckbox() {
+		const checkboxes = Array.from(this.form.querySelectorAll('input[type="checkbox"]'));
+
+		if (!checkboxes || !checkboxes.length) {
+			return;
 		}
+
+		checkboxes.forEach(checkbox => {
+			this.checkboxes.push(checkbox);
+		});
 	}
 
 	setButton(buttonSelector) {
